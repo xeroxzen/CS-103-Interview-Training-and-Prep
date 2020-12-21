d123e24eac3c8882a9ccae389903a950ec0adae0
xeroxzen <andilembele020@gmail.com>
2020-12-22 01:36:00 +0200

    Solving #3

--- ps1/q3.py
+++ ps1/q3.py
@@ -1,11 +1,13 @@
 def nth_int_without_nine(n):
-	"""
-	Given a positive integer n, you need to return the n-th integer after removing. 
-	Note that 1 will be the first integer.
+    """
+    Given a positive integer n, you need to return the n-th integer after removing. 
+    Note that 1 will be the first integer.
 
-	Check q3.md for a detailed description.
+    Check q3.md for a detailed description.
 
-	>>> print(nth_int_without_nine(9))
-	10
-	"""
-	pass
+    >>> print(nth_int_without_nine(9))
+    10
+    """
+
+    for i in range(1, 100):
+        return i
